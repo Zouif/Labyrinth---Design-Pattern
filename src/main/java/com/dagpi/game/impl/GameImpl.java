413625d2be1082e99751cc6e816c5966064c5ae5
Benjamin BARBE <contact@benjamin-barbe.fr>
2017-07-13 16:03:19 +0200

    Updated item and game packages

--- src/main/java/com/dagpi/game/impl/GameImpl.java
+++ src/main/java/com/dagpi/game/impl/GameImpl.java
@@ -1,6 +1,7 @@
 package com.dagpi.game.impl;
 
 import com.dagpi.character.Player;
+import com.dagpi.character.impl.PlayerImpl;
 import com.dagpi.game.Game;
 import com.dagpi.game.Level;
 
@@ -12,6 +13,10 @@ import java.util.ArrayList;
 public class GameImpl implements Game {
     private ArrayList<Level> levels;
     private Player player;
+
+    public GameImpl() {
+        player = new PlayerImpl();
+    }
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
--- src/main/java/com/dagpi/item/impl/KeyImpl.java
+++ src/main/java/com/dagpi/item/impl/KeyImpl.java
@@ -1,9 +1,15 @@
 package com.dagpi.item.impl;
 
+import com.dagpi.character.Player;
 import com.dagpi.item.Key;
 
 /**
  * Created by benjamin on 7/13/17.
  */
 public class KeyImpl implements Key {
+    public boolean interactWith(Character character) {
+        character.addToBag(this);
+
+        return true;
+    }
 }
